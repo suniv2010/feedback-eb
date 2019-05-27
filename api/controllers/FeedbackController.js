0e4992f3da759b664f2263f8c1561f80d4c7f72c
suvi271547 <sunithank.dvg@gmail.com>
2019-05-27 14:14:30 +0530

    add all the files

---
+++ api/controllers/FeedbackController.js
@@ -0,0 +1,43 @@
+/**
+ * ArticlesController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+    list:async function(req, res){
+      Feedback.find({}).exec(function(err,questions){
+            if(err){
+                  res.send(500,{error:'database error'});
+            }
+            console.log(questions);
+            res.view('questions/list', { questions: questions });
+           // res.view('articles/list', articles);
+            //res.view('/articles/list',{articles:articles});
+        })
+          
+    },
+    
+    add: function(req, res){
+      res.view('articles/add');
+  },
+  create:function(req, res){
+      //console.log(req.body);
+      var training = req.body.training;
+      var trainers = req.body.trainers;
+      var questions = req.body.questions;
+  
+      Feedback.create({training:training, trainers:trainers, questions:questions}).exec(function(err){
+          if(err){
+              res.send(500, {error: 'Database Error'});
+          }
+  
+         res.send("success");
+      });
+  },
+  
+  
+  };
+  
+  
---
+++ api/models/Feedback.js
@@ -0,0 +1,19 @@
+/**
+ * Articles.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    training: {type:'string'},
+    trainers: {type:'string'},
+    location:{type:'string'},
+   
+     questions: {type:'json'}
+},
+  datastores:'mongodb'
+};
+
